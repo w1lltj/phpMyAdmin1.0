84f46663b811f89c0c2cf4b85e733367ca8e82c4
w1lltj <31623323+w1lltj@users.noreply.github.com>
2017-09-11 20:57:53 +0800

    Update config.inc.php

--- config.inc.php
+++ config.inc.php
@@ -16,12 +16,12 @@ $i = 0;
 $i++;
 
 /* Authentication type and info */
-$cfg['Servers'][$i]['auth_type'] = 'config';
+/*$cfg['Servers'][$i]['auth_type'] = 'config';
 $cfg['Servers'][$i]['user'] = 'root';
 $cfg['Servers'][$i]['password'] = '';
 $cfg['Servers'][$i]['extension'] = 'mysqli';
 $cfg['Servers'][$i]['AllowNoPassword'] = true;
-$cfg['Lang'] = '';
+$cfg['Lang'] = '';*/
 
 /* Bind to the localhost ipv4 address and tcp */
 $cfg['Servers'][$i]['host'] = 'mysql';
